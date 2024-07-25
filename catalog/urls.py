2755a6bc0762be23f94c0af07a2673f575aa79af
Marc Altmann <marcaltmann@posteo.de>
2024-07-25 20:45:54 +0200

    Finish part 8

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("authors/", views.AuthorListView.as_view(), name="authors"),
     path("authors/<int:pk>", views.AuthorDetailView.as_view(), name="author-detail"),
     path("mybooks/", views.LoanedBooksByUserListView.as_view(), name="my-borrowed"),
+    path("borrowed/", views.BorrowedBooksLibrarianListView.as_view(), name="borrowed"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
 from django.views import generic
 
@@ -71,3 +71,14 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
             .filter(status__exact="o")
             .order_by("due_back")
         )
+
+
+class BorrowedBooksLibrarianListView(PermissionRequiredMixin, generic.ListView):
+    permission_required = 'catalog.can_mark_returned'
+
+    model = BookInstance
+    template_name = "catalog/bookinstance_list_borrowed_librarian.html"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact="o").order_by("due_back")
