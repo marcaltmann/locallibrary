04d4a14f911ab015dd63d60ce19fd73f48dfd22e
Marc Altmann <marcaltmann@posteo.de>
2024-08-05 08:57:41 +0200

    WIP part 9

--- catalog/urls.py
+++ catalog/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path("authors/<int:pk>", views.AuthorDetailView.as_view(), name="author-detail"),
     path("mybooks/", views.LoanedBooksByUserListView.as_view(), name="my-borrowed"),
     path("borrowed/", views.BorrowedBooksLibrarianListView.as_view(), name="borrowed"),
+    path(
+        "book/<uuid:pk>/renew/", views.renew_book_librarian, name="renew-book-librarian"
+    ),
 ]
