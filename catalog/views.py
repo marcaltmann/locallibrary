6c029a8c2cb061c79943e48c18753786a1f77370
Marc Altmann <marcaltmann@posteo.de>
2024-08-14 14:22:13 +0200

    Finish part 9

--- catalog/views.py
+++ catalog/views.py
@@ -4,11 +4,12 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from .forms import RenewBookForm
 from .models import Book, Author, BookInstance, Genre
-from catalog.forms import RenewBookForm
 
 
 def index(request):
@@ -121,3 +122,32 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, "catalog/book_renew_librarian.html", context)
+
+
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    model = Author
+    fields = ["first_name", "last_name", "date_of_birth", "date_of_death"]
+    initial = {"date_of_death": "11/11/2023"}
+    permission_required = "catalog.add_author"
+
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    model = Author
+    # Not recommended (potential security issue if more fields added)
+    fields = "__all__"
+    permission_required = "catalog.change_author"
+
+
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    model = Author
+    success_url = reverse_lazy("authors")
+    permission_required = "catalog.delete_author"
+
+    def form_valid(self, form):
+        try:
+            self.object.delete()
+            return HttpResponseRedirect(self.success_url)
+        except Exception as e:
+            return HttpResponseRedirect(
+                reverse("author-delete", kwargs={"pk": self.object.pk})
+            )
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -21,8 +21,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
-print(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
-
 urlpatterns = [
     path("", RedirectView.as_view(url="catalog/", permanent=True)),
     path("admin/", admin.site.urls),
